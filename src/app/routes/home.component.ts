80c7789129d53439dc0af580e63462204aa84767
thomas maestas <thomasm1.maestas@gmail.com>
2019-10-23 10:51:40 -0600

    service bug fixes

--- src/app/routes/home.component.ts
+++ src/app/routes/home.component.ts
@@ -26,8 +26,8 @@ export class HomeComponent implements OnInit {
                         var coins = ` 
 		  <li id="${r[i].Date}" class="bitDiv">  
 		  <h6><strong>${r[i].Symbol}:&nbsp;  ${r[i].Date} </strong> </h6>   
-		  <h6>Open: ${r[i].Open} &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Low: ${r[i].Low}</h6>
-		  <h6>Close: ${r[i].Close} &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; High: ${r[i].High}</h6> 
+		  <p>Open: ${r[i].Open}    &nbsp;&nbsp;&nbsp;&nbsp; Low: ${r[i].Low}</p>
+		  <p>Close: ${r[i].Close}  &nbsp;&nbsp;&nbsp;&nbsp; High: ${r[i].High}</p> 
 	<!--  <div>Volume BTC: ${r[i].BTC}
 		  </div>
 		  <div>Volume USD: ${r[i].USD}
@@ -62,77 +62,28 @@ console.log("Oops, no data ... Check your internet connection :]")
   ngOnInit() {
     this.prices = this._lambdaPriceService.getPrices();
     //this.getPrices();
-    this.initPromise();
-    this.initAsync1();
-    this.initAsync();
+    // this.initPromise();
+    // this.initAsync1();
+    // this.initAsync();
   }
-  arrayDelay = [
-    {
-      "Date": "2019-07-04",
-      "Symbol": "BTCUSD",
-      "Open": 11976.42,
-      "High": 12064.26,
-      "Low": 11820,
-      "Close": 11909.55,
-      "Volume BTC": 1237.57,
-      "Volume USD": 14790355.69
-    },
-    {
-      "Date": "2019-07-03",
-      "Symbol": "BTCUSD",
-      "Open": 10829.18,
-      "High": 12014.6,
-      "Low": 10829.18,
-      "Close": 11976.42,
-      "Volume BTC": 36836.85,
-      "Volume USD": 419076229.34
-    },
-    {
-      "Date": "2019-07-02",
-      "Symbol": "BTCUSD",
-      "Open": 10577.63,
-      "High": 10925,
-      "Low": 9651,
-      "Close": 10829.18,
-      "Volume BTC": 41476.42,
-      "Volume USD": 424791613.92
-    },
-    {
-      "Date": "2019-07-01",
-      "Symbol": "BTCUSD",
-      "Open": 10761.26,
-      "High": 11210.52,
-      "Low": 9953,
-      "Close": 10577.63,
-      "Volume BTC": 37633.9,
-      "Volume USD": 396857365.17
-    },
-    {
-      "Date": "2019-06-30",
-      "Symbol": "BTCUSD",
-      "Open": 11865.29,
-      "High": 12200,
-      "Low": 10650.06,
-      "Close": 10761.26,
-      "Volume BTC": 26505.91,
-      "Volume USD": 300275703.17
-    },
-    {
-      "Date": "2019-06-29",
-      "Symbol": "BTCUSD",
-      "Open": 12360.45,
-      "High": 12379.99,
-      "Low": 11318.94,
-      "Close": 11865.29,
-      "Volume BTC": 27300.42,
-      "Volume USD": 323053833.66
-    }
-  ];
-  
+
   getPriceList():any {
     console.log(this.prices);
     return this.prices;
   }
+//   arrayDelay = [
+//     {
+//    "Date": "2019-10-17",
+//    "Symbol": "BTCUSD",
+//    "Open": 7993.54,
+//    "High": 8070,
+//    "Low": 7937.01,
+//    "Close": 8034.32,
+//    "Volume BTC": 1570.08,
+//    "Volume USD": 12537245.82
+//  } 
+//   ];
+  
  // lambda-price.service.ts
  // price.model.ts
 // getPrices() {
@@ -151,81 +102,81 @@ console.log("Oops, no data ... Check your internet connection :]")
 //     return this.price;
 // }
 
-  afterTheDelay = function () {
-    console.log("afterTheDelay");
-    let outputPromise = "";
-    this.arrayDelay.forEach((arrayDelay, index) => {
-      outputPromise += `<div>Date: ${arrayDelay.Date} <br />Symbol:  ${arrayDelay.Symbol}<br /> Open: ${arrayDelay.Open} <br />Close: ${arrayDelay.Close}<hr></div>`;
-    });
-    document.getElementById("promises").innerHTML = "<strong>Historical Bitcoin:</strong><br />" + outputPromise;
-  }
+  // afterTheDelay = function () {
+  //   console.log("afterTheDelay");
+  //   let outputPromise = "";
+  //   this.arrayDelay.forEach((arrayDelay, index) => {
+  //     outputPromise += `<div>Date: ${arrayDelay.Date} <br />Symbol:  ${arrayDelay.Symbol}<br /> Open: ${arrayDelay.Open} <br />Close: ${arrayDelay.Close}<hr></div>`;
+  //   });
+  //   document.getElementById("promises").innerHTML = "<strong>Historical Bitcoin:</strong><br />" + outputPromise;
+  // }
 
-  addPromise = function (price) {
-    console.log("addPromise");
-    return new Promise((resolve, reject) => {
-      // setTimeout(() => {
-      this.arrayDelay.push(price);
-      // const error = true;
-      const error = false;
-      if (!error) {
-        resolve();
-      } else {
+  // addPromise = function (price) {
+  //   console.log("addPromise");
+  //   return new Promise((resolve, reject) => {
+  //     // setTimeout(() => {
+  //     this.arrayDelay.push(price);
+  //     // const error = true;
+  //     const error = false;
+  //     if (!error) {
+  //       resolve();
+  //     } else {
 
-        reject(console.log("%c Promise Errorrr occured", "color:orange; border:solid 1px orange"));
-      }
-      // }, 2000);
-    });
-  }
+  //       reject(console.log("%c Promise Errorrr occured", "color:orange; border:solid 1px orange"));
+  //     }
+  //     // }, 2000);
+  //   });
+  // }
 
-  initAsync = async function () {
-    console.log("initAsync");
-    await this.addPromise({
-      "Date": "2020-01-01",
-      "Symbol": "BTC",
-      "Open": 13000.00,
-      "High": 14000,
-      "Low": 12000.00,
-      "Close": 1350.00
-    })
-      .then(this.afterTheDelay)
-      .catch(err => console.log(err));
-    this.afterTheDelay();
-  }
+  // initAsync = async function () {
+  //   console.log("initAsync");
+  //   await this.addPromise({
+  //     "Date": "2020-01-01",
+  //     "Symbol": "BTC",
+  //     "Open": 13000.00,
+  //     "High": 14000,
+  //     "Low": 12000.00,
+  //     "Close": 1350.00
+  //   })
+  //     .then(this.afterTheDelay)
+  //     .catch(err => console.log(err));
+  //   this.afterTheDelay();
+  // }
 
 
 
-  promise1 = Promise.resolve("Price 1");
-  promise2 = 10;
-  promise3 = new Promise((resolve, reject) =>
-    setTimeout(resolve, 2000, "Timeout-2000ms")
-  );
-  promise4 = fetch(
-    "https://tmm-nov.s3.amazonaws.com/data/coins/BTCUSD_2019.json"
-  ).then(res => res.json());
+  // promise1 = Promise.resolve("Price 1");
+  // promise2 = 10;
+  // promise3 = new Promise((resolve, reject) =>
+  //   setTimeout(resolve, 2000, "Timeout-2000ms")
+  // );
+  // promise4 = fetch(
+  //   "https://tmm-nov.s3.amazonaws.com/data/coins/BTCUSD_2019.json"
+  // ).then(res => res.json());
 
-  initPromise = async function () {
-    Promise.all([this.promise1, this.promise2, this.promise3, this.promise4]).then(_values =>
-      console.log(_values)
-    );
-    Promise.all([this.promise1, this.promise2, this.promise3, this.promise4]).then(
-      values =>
+  // initPromise = async function () {
+  //   Promise.all([this.promise1, this.promise2, this.promise3, this.promise4]).then(_values =>
+  //     console.log(_values)
+  //   );
+  //   Promise.all([this.promise1, this.promise2, this.promise3, this.promise4]).then(
+  //     values =>
         
-      (document.getElementById("promiseAll").innerHTML =
-          "<strong>API: https://tmm-nov.s3.amazonaws.com/data/coins/BTCUSD_2019.json> </strong><br />   Date: " + values[3][1].Date + ' Symbol:' + values[3][1].Symbol + ' Close: ' + values[3][1].Close)
-    );
+  //     (document.getElementById("promiseAll").innerHTML =
+  //         "<strong>API: https://tmm-nov.s3.amazonaws.com/data/coins/BTCUSD_2019.json> </strong><br />   Date: " + values[3][1].Date + ' Symbol:' + values[3][1].Symbol + ' Close: ' + values[3][1].Close)
+  //   );
 
-  }
+  // }
 
-  initAsync1 = async function () {
-    await this.addPromise({
-      "Date": "2020-01-02",
-      "Symbol": "coinASYNCAdded",
-      "Open": 13000.00,
-      "High": 14000,
-      "Low": 12000.00,
-      "Close": 1350.00
-    });
-    this.afterTheDelay();
-  }
+  // initAsync1 = async function () {
+  //   await this.addPromise({
+  //     "Date": "2020-01-02",
+  //     "Symbol": "coinASYNCAdded",
+  //     "Open": 13000.00,
+  //     "High": 14000,
+  //     "Low": 12000.00,
+  //     "Close": 1350.00
+  //   });
+  //   this.afterTheDelay();
+  // }
 
 }
--- src/app/services/lambda-price.service.ts
+++ src/app/services/lambda-price.service.ts
@@ -28,25 +28,155 @@ export class LambdaPriceService {
 
   private listPrices: Price[] = //  
     [
-      { 
-   Date: "2019-10-17",
-   Symbol: "BTCUSD",
-   Open: 7993.54,
-   High: 8070,
-   Low: 7937.01,
-   Close: 8034.32,
-   VolumeBTC: 1570.08,
-   VolumeUSD: 12537245.82
+         {
+   "Date": "2019-10-17",
+   "Symbol": "BTCUSD",
+   "Open": 7993.54,
+   "High": 8070,
+   "Low": 7937.01,
+   "Close": 8034.32,
+   "VolumeBTC": 1570.08,
+   "VolumeUSD": 12537245.82
  },
  {
-   Date: "2019-10-16",
-   Symbol: "BTCUSD",
-   Open: 8162.44,
-   High: 8171.59,
-   Low: 7908.86,
-   Close: 7993.54,
-   VolumeBTC: 8123.72,
-   VolumeUSD: 65132771.81
+   "Date": "2019-10-16",
+   "Symbol": "BTCUSD",
+   "Open": 8162.44,
+   "High": 8171.59,
+   "Low": 7908.86,
+   "Close": 7993.54,
+   "VolumeBTC": 8123.72,
+   "VolumeUSD": 65132771.81
+ },
+ {
+   "Date": "2019-10-15",
+   "Symbol": "BTCUSD",
+   "Open": 8355,
+   "High": 8420,
+   "Low": 8085.65,
+   "Close": 8162.44,
+   "VolumeBTC": 7919.56,
+   "VolumeUSD": 65109952.98
+ },
+ {
+   "Date": "2019-10-14",
+   "Symbol": "BTCUSD",
+   "Open": 8282.97,
+   "High": 8409,
+   "Low": 8215.66,
+   "Close": 8355,
+   "VolumeBTC": 4401.82,
+   "VolumeUSD": 36575829.05
+ },
+ {
+   "Date": "2019-10-13",
+   "Symbol": "BTCUSD",
+   "Open": 8309.03,
+   "High": 8469.02,
+   "Low": 8146.47,
+   "Close": 8282.97,
+   "VolumeBTC": 5459.53,
+   "VolumeUSD": 45492326.33
+ },
+ {
+   "Date": "2019-10-12",
+   "Symbol": "BTCUSD",
+   "Open": 8267.33,
+   "High": 8425,
+   "Low": 8261.46,
+   "Close": 8309.03,
+   "VolumeBTC": 3001.53,
+   "VolumeUSD": 25023584.91
+ },
+ {
+   "Date": "2019-10-11",
+   "Symbol": "BTCUSD",
+   "Open": 8587.5,
+   "High": 8826,
+   "Low": 8226,
+   "Close": 8267.33,
+   "VolumeBTC": 11975.39,
+   "VolumeUSD": 101238415.25
+ },
+ {
+   "Date": "2019-10-10",
+   "Symbol": "BTCUSD",
+   "Open": 8590,
+   "High": 8660,
+   "Low": 8452.5,
+   "Close": 8587.5,
+   "VolumeBTC": 7892.41,
+   "VolumeUSD": 67531649.6
+ },
+ {
+   "Date": "2019-10-09",
+   "Symbol": "BTCUSD",
+   "Open": 8180,
+   "High": 8709,
+   "Low": 8121,
+   "Close": 8590,
+   "VolumeBTC": 13865.37,
+   "VolumeUSD": 117143751.99
+ },
+ {
+   "Date": "2019-10-08",
+   "Symbol": "BTCUSD",
+   "Open": 8209,
+   "High": 8342.97,
+   "Low": 8107.26,
+   "Close": 8180,
+   "VolumeBTC": 5808.84,
+   "VolumeUSD": 47674867.98
+ },
+ {
+   "Date": "2019-10-07",
+   "Symbol": "BTCUSD",
+   "Open": 7859.79,
+   "High": 8310.57,
+   "Low": 7762.35,
+   "Close": 8209,
+   "VolumeBTC": 9969.38,
+   "VolumeUSD": 80691163.82
+ },
+ {
+   "Date": "2019-10-06",
+   "Symbol": "BTCUSD",
+   "Open": 8147.63,
+   "High": 8176.18,
+   "Low": 7780.85,
+   "Close": 7859.79,
+   "VolumeBTC": 7770.16,
+   "VolumeUSD": 61753052.16
+ },
+ {
+   "Date": "2019-10-05",
+   "Symbol": "BTCUSD",
+   "Open": 8156.67,
+   "High": 8200,
+   "Low": 8020.2,
+   "Close": 8147.63,
+   "VolumeBTC": 3809.86,
+   "VolumeUSD": 30873507.65
+ },
+ {
+   "Date": "2019-10-04",
+   "Symbol": "BTCUSD",
+   "Open": 8240.41,
+   "High": 8241.35,
+   "Low": 8002.21,
+   "Close": 8156.67,
+   "VolumeBTC": 7620.06,
+   "VolumeUSD": 62078561.3
+ },
+ {
+   "Date": "2019-10-03",
+   "Symbol": "BTCUSD",
+   "Open": 8381.72,
+   "High": 8419.95,
+   "Low": 8059.22,
+   "Close": 8240.41,
+   "VolumeBTC": 7550.73,
+   "VolumeUSD": 62037926.73
  }
     ];
 
